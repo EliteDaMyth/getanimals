8978a25b47e1ca01d13bbaa0001f74335a1be4e3
Elite Myth <elitemythyt@gmail.com>
2020-01-12 18:00:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,76 +43,10 @@ exports.cat = function() {
     request.on('error', (err) => reject(err))
     })
 };
-exports.lion = function() {
-  // return new pending promise
-  return new Promise((resolve, reject) => {
-  		  let url = "https://get-animals.glitch.me/lion";
-    // select http or https module, depending on reqested url
-    const lib = url.startsWith('https') ? require('https') : require('http');
-    const request = lib.get(url, (response) => {
-      // handle http errors
-      if (response.statusCode < 200 || response.statusCode > 299) {
-         reject(new Error('Failed to load page, status code: ' + response.statusCode));
-       }
-      // temporary data holder
-      const body = [];
-      // on every content chunk, push it to the data array
-      response.on('data', (chunk) => body.push(chunk));
-      // we are done, resolve promise with those joined chunks
-      response.on('end', () => resolve(body.join('')));
-    });
-    // handle connection errors of the request
-    request.on('error', (err) => reject(err))
-    })
-};
-exports.tiger = function() {
-  // return new pending promise
-  return new Promise((resolve, reject) => {
-  		  let url = "https://get-animals.glitch.me/tiger";
-    // select http or https module, depending on reqested url
-    const lib = url.startsWith('https') ? require('https') : require('http');
-    const request = lib.get(url, (response) => {
-      // handle http errors
-      if (response.statusCode < 200 || response.statusCode > 299) {
-         reject(new Error('Failed to load page, status code: ' + response.statusCode));
-       }
-      // temporary data holder
-      const body = [];
-      // on every content chunk, push it to the data array
-      response.on('data', (chunk) => body.push(chunk));
-      // we are done, resolve promise with those joined chunks
-      response.on('end', () => resolve(body.join('')));
-    });
-    // handle connection errors of the request
-    request.on('error', (err) => reject(err))
-    })
-};
-exports.panda = function() {
-  // return new pending promise
-  return new Promise((resolve, reject) => {
-  		  let url = "https://get-animals.glitch.me/panda";
-    // select http or https module, depending on reqested url
-    const lib = url.startsWith('https') ? require('https') : require('http');
-    const request = lib.get(url, (response) => {
-      // handle http errors
-      if (response.statusCode < 200 || response.statusCode > 299) {
-         reject(new Error('Failed to load page, status code: ' + response.statusCode));
-       }
-      // temporary data holder
-      const body = [];
-      // on every content chunk, push it to the data array
-      response.on('data', (chunk) => body.push(chunk));
-      // we are done, resolve promise with those joined chunks
-      response.on('end', () => resolve(body.join('')));
-    });
-    // handle connection errors of the request
-    request.on('error', (err) => reject(err))
-    })
-};
 exports.bird = function() {
   // return new pending promise
   return new Promise((resolve, reject) => {
-  		  let url = "https://get-animals.glitch.me/birb";
+  		  let url = "https://get-animals.glitch.me/bird";
     // select http or https module, depending on reqested url
     const lib = url.startsWith('https') ? require('https') : require('http');
     const request = lib.get(url, (response) => {
@@ -131,10 +65,10 @@ exports.bird = function() {
     request.on('error', (err) => reject(err))
     })
 };
-exports.penguin = function() {
+exports.shiba = function() {
   // return new pending promise
   return new Promise((resolve, reject) => {
-  		  let url = "https://get-animals.glitch.me/penguin";
+  		  let url = "https://get-animals.glitch.me/shiba";
     // select http or https module, depending on reqested url
     const lib = url.startsWith('https') ? require('https') : require('http');
     const request = lib.get(url, (response) => {
@@ -153,10 +87,10 @@ exports.penguin = function() {
     request.on('error', (err) => reject(err))
     })
 };
-exports.duck = function() {
+exports.fox = function() {
   // return new pending promise
   return new Promise((resolve, reject) => {
-  		  let url = "https://get-animals.glitch.me/duck";
+  		  let url = "https://get-animals.glitch.me/fox";
     // select http or https module, depending on reqested url
     const lib = url.startsWith('https') ? require('https') : require('http');
     const request = lib.get(url, (response) => {
