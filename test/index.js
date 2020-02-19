99987983732f309c9029f34f302f7039bd44eecb
Elite Myth <elitemythyt@gmail.com>
2020-02-19 16:33:01 +0530

    Delete index.js

--- test/index.js
+++
@@ -1,5 +0,0 @@
-var getanimals = require('../index.js');
-
-getanimals.dog() // you can use any other animal ex getanimals.cat() etc
-  .then((url) => console.log(url))
-  .catch((err) => console.error(err));
